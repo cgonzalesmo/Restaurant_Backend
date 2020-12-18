4120320a56ef7e8c4c3700a0ddc004084a983683
cgonzalesmo <cgonzalesmo@unsa.edu.pe>
2020-12-17 22:52:22 -0500

    Subiendo CORS

--- Restaurant_Backend/settings.py
+++ Restaurant_Backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     'gestionPersonal',
     'gestionProductos',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,5 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000"
+]
 
 AUTH_USER_MODEL = "gestionPersonal.G1_USUARIO"
