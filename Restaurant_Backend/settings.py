90c48e946cb4f0f18784ef98635d31ee350e5fbc
cgonzalesmo <58450963+cgonzalesmo@users.noreply.github.com>
2020-12-21 22:04:33 -0500

    Update settings.py

--- Restaurant_Backend/settings.py
+++ Restaurant_Backend/settings.py
@@ -86,15 +86,8 @@ WSGI_APPLICATION = 'Restaurant_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sistema_restaurant',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
